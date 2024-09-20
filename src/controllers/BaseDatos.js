dd0ff2a3f9a2f4ab7d142ec9a0d560b201977077
rulossan <165792347+rulossan@users.noreply.github.com>
2024-09-20 07:24:43 -0500

    se crea archivo para emular bs

---
+++ src/controllers/BaseDatos.js
@@ -0,0 +1,14 @@
+let gatosDos = Array(
+    {
+        id:1,
+        descrpicion:"Pasaje de sabaneta a cesde",
+        monto:8000,
+        fecha:"2024-9-20"
+    },
+    {
+        id:2,
+        descrpicion:"Empanada de pollo + cocacola zero",
+        monto:10000,
+        fecha:"2024-9-20"
+    },
+)
